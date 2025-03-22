102836147e8fb39c5bce1920daccdfa906ec8a0d
CristiSx <cristiansalafenco@gmail.com>
2025-03-22 22:54:24 +0200

    Am rezolvat problea cu prev

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './index.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mainpage from "./views/mainpage"
 import Prev1 from './views/CVs/prev1';
+import Prev2 from './views/CVs/prev2';
 
 function App() {
 
@@ -11,6 +12,8 @@ function App() {
     <Routes>
       <Route path="/mainpage" element={<Mainpage/>} />
       <Route path='/mainpage/prev1' element={<Prev1/>} />
+      <Route path='/mainpage/prev2' element={<Prev2/>} />
+
     </Routes>
     </BrowserRouter>
   )
